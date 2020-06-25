6413dfac07f9aa8cc7ef49aa4ce334c231c80ea8
innocent <paulinnocent04@gmail.com>
2020-06-25 13:09:59 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+
+if __name__ == '__main__':
+    main()
