1eb3c45d6918b1de28ea9f53ca818277592c444d
innocent <paulinnocent04@gmail.com>
2020-06-27 12:58:09 +0100

    index fixed

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
   $(".login-btn").on("click", function () {
     $(".login-modal").css("display", "flex");
   })
-  $(".login-close").on("click", function () {
-    $(".login-modal").css("display", "none");
+  $(".modal-close").on("click", function () {
+    $(".modal").css("display", "none");
   })
 
   $(".reg-btn").on("click", function () {
     $(".reg-modal").css("display", "flex");
   })
-  $(".reg-close").on("click", function () {
-    $(".reg-modal").css("display", "none");
+  $(".modal-close").on("click", function () {
+    $(".modal").css("display", "none");
   })
 })
