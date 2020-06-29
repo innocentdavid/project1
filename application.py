7b12fb31d9c24dc02657193be4e0bd9df4dd2acb
innocent <paulinnocent04@gmail.com>
2020-06-29 20:56:26 +0100

    mnbkn

--- application.py
+++ application.py
@@ -2,8 +2,8 @@ import os
 #import requests
 from flask import (
     Flask,
-    g,
     session,
+    g,
     redirect,
     request,
     render_template,
@@ -17,14 +17,13 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
-app.secret_key = "secretkey"
+app.secret_key = os.urandom(24)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://tibchumrwgmuym:767d5d8610d692050757f686ab151d2c2f214a7f7bb00cfe2d81698226bb72d8@ec2-34-230-231-71.compute-1.amazonaws.com:5432/dbuj3hhsofrf5f'
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
@@ -33,17 +32,6 @@ Session(app)
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
-
-
-class User:
-
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return f"<user: {self.username} added!>"
 
 
 @app.route("/")
@@ -58,26 +46,26 @@ def index():
 def login():
     if request.method == "POST":
         session.pop('uid', None)
+
         username = request.form["username"]
         password = request.form["password"]
 
-        # users = []
-        # users.append(User(id=1, username="admin", password="admin"))
 
-        Userz = db.execute(
-            "SELECT * FROM users WHERE username=:username", {"username": username}).fetchall()
-        for user in Userz:
-            print(f"user: {user.id} | {user.username}")
-            if user.username == username:
-                return "username exist"
-            if user.password == password:
-                session['uid'] = user.id
-                return "Login successful!"
-            if not user.password == password:
+        Users = db.execute(
+            "SELECT * FROM users WHERE username = :username", {"username": username}).fetchall()
+        
+        for user in Users:
+            print(f"<user: {user.id} | {user.username} | {user.password} VS {username} and {password}")
+            
+            if username == f"{user.username}" :
+                
+                if password == "admin" :
+                    
+                    session['uid'] = user.id
+                    return "Login successful!"
                 return "Password incorrect! please try again"
+            return "username not found! please try again or register"
 
-        return f"{username}? not found! recheck or register"
-        return "d"
     return "invalid!"
 
 
