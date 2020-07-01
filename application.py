ff406f4ff8ec0048ee9108dda3b8c947e2223614
innocent <paulinnocent04@gmail.com>
2020-07-01 09:22:58 +0100

    updated

--- application.py
+++ application.py
@@ -1,31 +1,36 @@
 import os
-# import requests
+
 from flask import (
     Flask,
-    session,
     g,
     redirect,
-    request,
     render_template,
+    request,
+    session,
     url_for
 )
+
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-class UserC:
+
+class User:
     def __init__(self, id, username, password):
         self.id = id
         self.username = username
         self.password = password
 
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='Anthony', password='password'))
+users.append(User(id=2, username='Becca', password='secret'))
+users.append(User(id=3, username='Carlos', password='somethingsimple'))
+
 
 app = Flask(__name__)
-
-app.secret_key = os.urandom(24)
-
-app.config["DEBUG"] = True
-
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -40,36 +45,62 @@ Session(app)
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+app.secret_key = 'somesecretkeythatonlyishouldknow'
 
-@app.route("/")
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        UserD = db.execute("SELECT * FROM users").fetchall()
+
+        for userr in UserD:
+            users.append(User(id=userr.id, username=userr.username, password=userr.password))
+
+        for user in users:
+            if user.id == session['user_id']:
+                g.user = user
+        
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if not g.user:
+        return redirect(url_for('login'))
 
     books = db.execute("SELECT * FROM books LIMIT 50").fetchall()
 
     return render_template('index.html', books=books)
 
 
-@app.route("/login", methods=["GET", "POST"])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == "POST":
+    if request.method == 'POST':
+        session.pop('user_id', None)
 
-        session.pop('uid', None)
+        username = request.form['username']
+        password = request.form['password']
 
-        username = request.form["username"]
-        password = request.form["password"]
-
-        UserD = db.execute(
-            "SELECT * FROM users WHERE username = :username", {"username": username}).fetchall()
-
+        print(users)
         
-        for user in UserD:
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('index'))
 
-            if user and user.password == password:
-                session['uid'] = user.id
-                g.user = user
-                return "Login successful!"
-            return "Password incorrect! please try again"
-        return "username does not exist try again or register"
-    return "invalid!"
+        return redirect(url_for('login'))
 
-# @ap.route("/single", methods["GET", "POST"])
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    if not g.user:
+        return redirect(url_for('login'))
+
+    return render_template('profile.html')
+
+@app.route('/logout')
+def logout():
+    if g.user:
+        session.pop('user_id', None)
+        return redirect(url_for('login'))
+
+    return redirect(url_for('login'))
