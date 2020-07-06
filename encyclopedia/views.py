90882eda4cb31088ba87390ecfbefb5215393716
innocentdavid <paulinnocent04@gmail.com>
2020-07-06 11:39:56 +0100

    d

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -7,4 +7,9 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+      "entry": util.get_entry(title)
+    })
 
